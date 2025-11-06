4ef165863e51a81b4f0e2a8a878e9e27469545d7
Yohaan <yohanakipruto@gmail.com>
2025-11-06 21:17:55 +0300

    Added BeachSpa page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import MainLayout from './layouts/MainLayout.jsx'
 import HomePage from './pages/HomePage.jsx'
 import LearnPage from './pages/LearnPage.jsx'
 import GetIntouch from './pages/GetIntouch.jsx'
+import BeachSpa from './pages/BeachSpa.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<HomePage />} />
       <Route path='/learn' element={<LearnPage/> } />
-      <Route path='/get-intouch' element={<GetIntouch/> } />
+      <Route path='/get-intouch' element={<GetIntouch />} />
+      <Route path='/Beach-Spa' element={<BeachSpa/> } />
       </Route>
   ));
 const App = () => {
